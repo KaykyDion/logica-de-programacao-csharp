3951795bb10b8dc588f1018d75cadd8fc67d3396
Kayky Dion <kayky.dion@gmail.com>
2025-03-17 16:21:55 -0300

    1002, 1003, 1004, 1007, 1008, 1010 concluídos

---
+++ 1008-salario/1008-salario/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace _1008_salario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int id, hoursWorked;
+            double hourlyRate, salary;
+
+            id = int.Parse(Console.ReadLine());
+            hoursWorked = int.Parse(Console.ReadLine());
+
+            hourlyRate = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            salary = hourlyRate * hoursWorked;
+
+            Console.WriteLine("NUMBER = " + id);
+            Console.WriteLine("SALARY = U$ " + salary.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
---
+++ 1010-calculo-simples/1010-calculo-simples/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace _1010_calculo_simples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] firstLine = Console.ReadLine().Split(' ');
+            string[] secondLine = Console.ReadLine().Split(' ');
+
+            double firstPrice = double.Parse(firstLine[2], CultureInfo.InvariantCulture);
+            double secondPrice = double.Parse(secondLine[2], CultureInfo.InvariantCulture);
+
+            int firstAmount = int.Parse(firstLine[1]);
+            int secondAmount = int.Parse(secondLine[1]);
+
+            double fullPrice = firstPrice * firstAmount + secondPrice * secondAmount;
+            
+            Console.WriteLine("VALOR A PAGAR: R$ " + fullPrice.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
