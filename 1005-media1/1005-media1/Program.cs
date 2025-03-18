064167a2f5267c205b0c7ce5235fbe0065933a46
Kayky Dion <kayky.dion@gmail.com>
2025-03-18 18:00:25 -0300

    1005, 1006 consluídos

---
+++ 1005-media1/1005-media1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace _1005_media1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double A, B, heightA, heightB, average;
+            heightA = 3.5;
+            heightB = 7.5;
+
+            A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            average = ((A * heightA) + (B * heightB)) / (heightA + heightB);
+
+            Console.WriteLine("MEDIA = " + average.ToString("F5", CultureInfo.InvariantCulture));
+        }
+    }
+}
---
+++ 1006-media2/1006-media2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace _1006_media2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double A, B, C, heightA, heightB, heightC, heightSum, average;
+
+            heightA = 2;
+            heightB = 3;
+            heightC = 5;
+            heightSum = heightA + heightB + heightC;
+
+            A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            average = ((A * heightA) + (B * heightB) + (C * heightC)) / heightSum;
+
+            Console.WriteLine("MEDIA = " + average.ToString("F1", CultureInfo.InvariantCulture));
+        }
+    }
+}
